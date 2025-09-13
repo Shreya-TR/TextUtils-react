a6279cbf90ba9a2ac49896d7afdde7ab76b53f75
Shreya-TR <shreya465tr@gmail.com>
2025-09-13 19:23:28 +0530

    created TextForm using State & Handling events

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
-
+import TextForm from './components/TextForm';
 // let name = "shreya";
 function App() {
   return (
     <>
     {/* <div className="blank"><h5>Hello {name}</h5></div> */}
     <Navbar title="TextUtils" aboutText="About Textutils" />
+    {/* my-3 -> provide space up and down */}
+    <div className="container my-3">
+    <TextForm heading="Enter the text to analyze" />
+    </div>
+
     </>
   );
 }
